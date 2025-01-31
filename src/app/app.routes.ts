1d947623efd007ddf4797260f89862e74765767f
Daniel Mudry <danimudry@gmail.com>
2025-01-31 16:39:40 -0300

    uno

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { MapaComponent } from './pages/mapa/mapa.component';
+import { CountryViewComponent } from './pages/country-view/country-view.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'mapas' },
+  { path: 'mapas', component: MapaComponent },
+  { path: 'countrie/:code', component: CountryViewComponent },
+];
