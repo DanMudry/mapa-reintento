f7611eef0842af97e1852ff935d6ab43acce4005
Daniel Mudry <danimudry@gmail.com>
2025-02-01 19:46:34 -0300

    configuracion ya hecha

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { api } from '../api';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'mapas';
+  ngOnInit(): void {
+    const script = document.createElement('script');
+    script.type = 'text/javascript';
+    script.src = `https://maps.googleapis.com/maps/api/js?key=${api}`;
+    script.async = true;
+    script.defer = true;
+    document.head.appendChild(script);
+  }
 }
