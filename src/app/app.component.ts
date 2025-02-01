15807a013ad14d9c4f1afe243618ce16a9898b60
Daniel Mudry <danimudry@gmail.com>
2025-02-01 20:31:03 -0300

    CARGO EL MAPA

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,14 +9,14 @@ import { api } from '../api';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
   title = 'mapas';
-  ngOnInit(): void {
+  /*ngOnInit(): void {
     const script = document.createElement('script');
     script.type = 'text/javascript';
     script.src = `https://maps.googleapis.com/maps/api/js?key=${api}`;
     script.async = true;
     script.defer = true;
     document.head.appendChild(script);
-  }
+  }*/
 }
--- src/app/pages/mapa/mapa.component.ts
+++ src/app/pages/mapa/mapa.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
+import { GoogleMap, MapMarker } from '@angular/google-maps';
 
 @Component({
   selector: 'app-mapa',
   standalone: true,
-  imports: [],
+  imports: [GoogleMap, MapMarker],
   templateUrl: './mapa.component.html',
-  styleUrl: './mapa.component.css'
+  styleUrl: './mapa.component.css',
 })
-export class MapaComponent {
-
-}
+export class MapaComponent {}
