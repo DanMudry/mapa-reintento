3e29c322e5f81964715506a9f4f434d09013cc14
Daniel Mudry <danimudry@gmail.com>
2025-02-04 18:26:50 -0300

    GET ALL

--- src/app/interfaces/icountry.interface.ts
+++ src/app/interfaces/icountry.interface.ts
@@ -1,2 +1,23 @@
 export interface ICountry {
+  name: any;
+  tld: string[];
+  cca2: string;
+  ccn3: string;
+  cca3: string;
+  cioc: string;
+  independent: boolean;
+  status: string;
+  unMember: boolean;
+  currencies: any;
+  idd: any;
+  capital: string;
+  altSpellings: string[];
+  region: string;
+  subregion: string;
+  languages: any;
+  translations: any;
+  landlocked: boolean;
+  area: number;
+  demonyms: any;
+  flag: string;
 }
--- src/app/services/countries.service.ts
+++ src/app/services/countries.service.ts
@@ -1,9 +1,18 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { ICountry } from '../interfaces/icountry.interface';
+import { lastValueFrom } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CountriesService {
+  private baseUrl: string = 'https://restcountries.com/v3.1/region/';
 
-  constructor() { }
+  private httpCliente = inject(HttpClient);
+  getAll(region = 'europe'): Promise<ICountry[]> {
+    return lastValueFrom(
+      this.httpCliente.get<ICountry[]>(`${this.baseUrl}${region}`)
+    );
+  }
 }
